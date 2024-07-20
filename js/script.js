7f785437b5168d9f37a218c47f8d02a047422695
Jackson Ferreira <jackson_planovip@hotmail.com>
2024-07-20 12:22:57 -0300

    feat: Adicionar seleção de elementos com jQuery
    Adiciona seleção de elementos por tag, classe e id utilizando jQuery.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@ $(document).ready(() => {
     });
 });
 
+
+// Seleção de elementos com jQuery
+
+/* 
+    Selecione elementos por tag - $('tag')
+    Selecione elementos por classe - $('.classe')
+    Selecione elementos por id - $('#id')
+*/
+
+console.log( $('h1') );
+console.log( $('div') );
+console.log( $('ul') );
+console.log( $('li') );
+console.log( $('input') );
+console.log( $('select') );
+
+console.log( $('.lista1'));
+console.log( $('#img'));
+
+
+
