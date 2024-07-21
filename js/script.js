794bbcfd61e434e43c719b751eac4b8dc954c3dd
Jackson Ferreira <jackson_planovip@hotmail.com>
2024-07-20 21:00:22 -0300

    Combinação de seletores com jQuery

--- js/script.js
+++ js/script.js
@@ -25,5 +25,16 @@ console.log( $('select') );
 console.log( $('.lista1'));
 console.log( $('#img'));
 
+// Combinação de seletores
+    // Selecione elementos por tag específica dentro de um elemento - $('elemento tag')
+console.log( $('input.exemplo1')) ;
+    // Selecionar elementos por classe de classe específica dentro de um elemento - $('elemento .classe')
+console.log( $('.lista1 .exemplo1')) ;
+console.log( $('.lista2 .exemplo1')) ;
 
+// Atuação sobre o array de retorno dos elementos selecionados
 
+console.log( $('.lista1 .exemplo1:first') ); // Primeiro elemento
+console.log( $('.lista1 .exemplo1:last') ); // Último elemento
+console.log( $('.lista1 .exemplo1:even') ); // Elementos pares
+console.log( $('.lista1 .exemplo1:odd') ); // Elementos ímpares
